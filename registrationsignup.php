edfb2b421e004c71aed5d239b95d1f579821fcc3
mwendwa-g <geraldmwendwa72@gmail.com>
2024-11-25 15:30:09 +0300

    Update registrationsignup.php

--- registrationsignup.php
+++ registrationsignup.php
@@ -2,27 +2,6 @@
     include("databaseconnection.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First SignUp</title>
-</head>
-<body>
-    <form action="<?php
-    htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
-        <h2>My first signup</h2><br>
-        username:<br>
-        <input type="text" name="username"><br>
-        password:<br>
-        <input type="password" name="password"><br>
-        <input type="submit" name="submit" value="register ">
-
-    </form>
-</body>
-</html>
-
 
 <?php
 
